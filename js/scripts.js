1fb364ab36fb44e940d45a5d941e2d9488dcc17a
Kyle Lange <baronsintrees@gmail.com>
2016-09-30 16:02:02 -0700

    add html structure for the nav bar and bottom bar on all pages. v0.1

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function Comment (name, email, comment){
 $(document).ready(function(){
   $("#form-comment").submit(function(){
     event.preventDefault();
-    $("#output").hide();
+    $("#output").reset();
     var inputName = $("#name-input").val();
     var inputEmail = $("#email-input").val();
     var inputComment = $("#comment-input").val();
@@ -18,7 +18,7 @@ $(document).ready(function(){
     databases.push(newComment);
 
     $("#output").append("<h2> Thanks "+newComment.name+", we will be in touch soon.</h2>")
-    $("#output").slideDown(1000);
+    $("#output").slideDown(1500);
     $("#output").slideUp(1500);
 
   });
