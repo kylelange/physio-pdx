e87fa44fec12cea0b94199e593226d3fc8369624
Kyle Lange <baronsintees@gmail.com>
2016-09-30 15:03:31 -0700

    add javaScript functionality to the index page submit button to a global array. DOM div also added to show input recieved.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
 // Business Logic
-
-
+var databases = [];
+function Comment (name, email, comment){
+  this.name = name;
+  this.email = email;
+  this.comment = comment;
+}
 
 // User Interface Logic
 $(document).ready(function(){
+  $("#form-comment").submit(function(){
+    event.preventDefault();
+    $("#output").hide();
+    var inputName = $("#name-input").val();
+    var inputEmail = $("#email-input").val();
+    var inputComment = $("#comment-input").val();
+    var newComment = new Comment(inputName, inputEmail, inputComment);
+    databases.push(newComment);
 
+    $("#output").append("<h2> Thanks "+newComment.name+", we will be in touch soon.</h2>")
+    $("#output").slideDown(1000);
+    $("#output").slideUp(1500);
+
+  });
+
+// lookup answer on j'ena's files
+  $("#myCarousel").click(function(){
+
+  });
 });
